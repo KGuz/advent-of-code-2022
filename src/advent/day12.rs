4b2e31290cdc68aab9a87d2c470191c28c75cc5c
KGuz <kamilguz0@gmail.com>
2023-12-04 22:37:16 +0100

    feat: add examples and descriptions

--- src/advent/day12.rs
+++ src/advent/day12.rs
@@ -1,5 +1,105 @@
 use super::*;
-use std::collections::{HashMap, hash_map::Entry};
+use std::collections::{hash_map::Entry, HashMap};
+
+pub struct Day12 {
+    /* --- Day 12: Hill Climbing Algorithm ---
+    You try contacting the Elves using your handheld device, but the river
+    you're following must be too low to get a decent signal.
+
+    You ask the device for a heightmap of the surrounding area (your puzzle
+    input). The heightmap shows the local area from above broken into a grid;
+    the elevation of each square of the grid is given by a single lowercase
+    letter, where a is the lowest elevation, b is the next-lowest, and so on up
+    to the highest elevation, z.
+
+    Also included on the heightmap are marks for your current position (S) and
+    the location that should get the best signal (E). Your current position (S)
+    has elevation a, and the location that should get the best signal (E) has
+    elevation z.
+
+    You'd like to reach E, but to save energy, you should do it in as few steps
+    as possible. During each step, you can move exactly one square up, down,
+    left, or right. To avoid needing to get out your climbing gear, the
+    elevation of the destination square can be at most one higher than the
+    elevation of your current square; that is, if your current elevation is m,
+    you could step to elevation n, but not to elevation o. (This also means
+    that the elevation of the destination square can be much lower than the
+    elevation of your current square.)
+
+    For example:
+
+    Sabqponm
+    abcryxxl
+    accszExk
+    acctuvwj
+    abdefghi
+
+    Here, you start in the top-left corner; your goal is near the middle. You
+    could start by moving down or right, but eventually you'll need to head
+    toward the e at the bottom. From there, you can spiral around to the goal:
+
+    v..v<<<<
+    >v.vv<<^
+    .>vv>E^^
+    ..v>>>^^
+    ..>>>>>^
+
+    In the above diagram, the symbols indicate whether the path exits each
+    square moving up (^), down (v), left (<), or right (>). The location
+    that should get the best signal is still E, and . marks unvisited squares.
+
+    This path reaches the goal in 31 steps, the fewest possible.
+
+    What is the fewest steps required to move from your current position to the
+    location that should get the best signal?
+
+    --- Part Two ---
+    As you walk up the hill, you suspect that the Elves will want to turn this
+    into a hiking trail. The beginning isn't very scenic, though; perhaps you
+    can find a better starting point.
+
+    To maximize exercise while hiking, the trail should start as low as
+    possible: elevation a. The goal is still the square marked E. However, the
+    trail should still be direct, taking the fewest steps to reach its goal.
+    So, you'll need to find the shortest path from any square at elevation a to
+    the square marked E.
+
+    Again consider the example from above:
+
+    Sabqponm
+    abcryxxl
+    accszExk
+    acctuvwj
+    abdefghi
+
+    Now, there are six choices for starting position (five marked a, plus the
+    square marked S that counts as being at elevation a). If you start at the
+    bottom-left square, you can reach the goal most quickly:
+    ...v<<<<
+    ...vv<<^
+    ...v>E^^
+    .>v>>>^^
+    >^>>>>>^
+
+    This path reaches the goal in only 29 steps, the fewest possible.
+
+    What is the fewest steps required to move starting from any square with
+    elevation a to the location that should get the best signal? */
+}
+
+impl Puzzle for Day12 {
+    fn part_one(&self, data: &'static str) -> String {
+        let heightmap = HeightMap::from(data);
+        let path = bfs(&heightmap.graph, heightmap.src, heightmap.dst);
+        (path.unwrap().len() - 1).to_string()
+    }
+
+    fn part_two(&self, data: &'static str) -> String {
+        let heightmap = HeightMap::from(data);
+        let path = bfs_reversed(&heightmap.graph, heightmap.dst, b'a');
+        (path.unwrap().len() - 1).to_string()
+    }
+}
 
 type Point = (usize, usize);
 type Graph = Vec<Vec<u8>>;
@@ -16,26 +116,17 @@ impl HeightMap {
 
         for (y, row) in graph.iter_mut().enumerate() {
             for (x, val) in row.iter_mut().enumerate() {
-                if *val == b'S' { *val = b'a'; src = (y, x)}
-                if *val == b'E' { *val = b'z'; dst = (y, x)}
+                if *val == b'S' {
+                    *val = b'a';
+                    src = (y, x)
+                }
+                if *val == b'E' {
+                    *val = b'z';
+                    dst = (y, x)
+                }
             }
         }
         Self { graph, src, dst }
-    }
-}
-
-pub struct Day12 {}
-impl Puzzle for Day12 {
-    fn part_one(&self, data: &'static str) -> String {
-        let heightmap = HeightMap::from(data);
-        let path = bfs(&heightmap.graph, heightmap.src, heightmap.dst);
-        (path.unwrap().len() - 1).to_string()
-    }
-
-    fn part_two(&self, data: &'static str) -> String {
-        let heightmap = HeightMap::from(data);
-        let path = bfs_reversed(&heightmap.graph, heightmap.dst, b'a');
-        (path.unwrap().len() - 1).to_string()
     }
 }
 
@@ -43,21 +134,31 @@ fn neighbours(graph: &Graph, p: Point) -> Vec<Point> {
     let (h, w) = (graph.len() - 1, graph[0].len() - 1);
     let mut neighbours = vec![];
 
-    if p.0 > 0 { neighbours.push((p.0 - 1, p.1)) }
-    if p.0 < h { neighbours.push((p.0 + 1, p.1)) }
-    if p.1 > 0 { neighbours.push((p.0, p.1 - 1)) }
-    if p.1 < w { neighbours.push((p.0, p.1 + 1)) }
+    if p.0 > 0 {
+        neighbours.push((p.0 - 1, p.1))
+    }
+    if p.0 < h {
+        neighbours.push((p.0 + 1, p.1))
+    }
+    if p.1 > 0 {
+        neighbours.push((p.0, p.1 - 1))
+    }
+    if p.1 < w {
+        neighbours.push((p.0, p.1 + 1))
+    }
     neighbours
 }
 
 fn attainable_neighbours(graph: &Graph, p: Point, f: impl Fn(u8, u8) -> bool) -> Vec<Point> {
     let neighbours = neighbours(graph, p).into_iter();
-    neighbours.filter(|&n| f(graph[p.0][p.1], graph[n.0][n.1])).collect()
+    neighbours
+        .filter(|&n| f(graph[p.0][p.1], graph[n.0][n.1]))
+        .collect()
 }
 
 fn bfs(graph: &Graph, src: Point, dst: Point) -> Option<Vec<Point>> {
     let mut visited = map![(src, None)];
-    let mut queue =  queue![src];
+    let mut queue = queue![src];
 
     while let Some(current) = queue.pop_front() {
         if current == dst {
@@ -76,7 +177,7 @@ fn bfs(graph: &Graph, src: Point, dst: Point) -> Option<Vec<Point>> {
 
 fn bfs_reversed(graph: &Graph, src: Point, dst: u8) -> Option<Vec<Point>> {
     let mut visited = map![(src, None)];
-    let mut queue =  queue![src];
+    let mut queue = queue![src];
 
     while let Some(current) = queue.pop_front() {
         if graph[current.0][current.1] == dst {
--- src/advent/day18.rs
+++ src/advent/day18.rs
@@ -3,6 +3,87 @@ use itertools::Itertools;
 use pt::P3;
 use std::collections::{HashSet, VecDeque};
 
+pub struct Day18 {
+    /* --- Day 18: Boiling Boulders ---
+    You and the elephants finally reach fresh air. You've emerged near the base
+    of a large volcano that seems to be actively erupting! Fortunately, the
+    lava seems to be flowing away from you and toward the ocean.
+
+    Bits of lava are still being ejected toward you, so you're sheltering in
+    the cavern exit a little longer. Outside the cave, you can see the lava
+    landing in a pond and hear it loudly hissing as it solidifies.
+
+    Depending on the specific compounds in the lava and speed at which it
+    cools, it might be forming obsidian! The cooling rate should be based on
+    the surface area of the lava droplets, so you take a quick scan of a
+    droplet as it flies past you (your puzzle input).
+
+    Because of how quickly the lava is moving, the scan isn't very good; its
+    resolution is quite low and, as a result, it approximates the shape of the
+    lava droplet with 1x1x1 cubes on a 3D grid, each given as its x,y,z
+    position.
+
+    To approximate the surface area, count the number of sides of each cube
+    that are not immediately connected to another cube. So, if your scan were
+    only two adjacent cubes like 1,1,1 and 2,1,1, each cube would have a single
+    side covered and five sides exposed, a total surface area of 10 sides.
+
+    Here's a larger example:
+
+    2,2,2
+    1,2,2
+    3,2,2
+    2,1,2
+    2,3,2
+    2,2,1
+    2,2,3
+    2,2,4
+    2,2,6
+    1,2,5
+    3,2,5
+    2,1,5
+    2,3,5
+
+    In the above example, after counting up all the sides that aren't connected
+    to another cube, the total surface area is 64.
+
+    What is the surface area of your scanned lava droplet?
+
+    --- Part Two ---
+    Something seems off about your calculation. The cooling rate depends on
+    exterior surface area, but your calculation also included the surface area
+    of air pockets trapped in the lava droplet.
+
+    Instead, consider only cube sides that could be reached by the water and
+    steam as the lava droplet tumbles into the pond. The steam will expand to
+    reach as much as possible, completely displacing any air on the outside of
+    the lava droplet but never expanding diagonally.
+
+    In the larger example above, exactly one cube of air is trapped within the
+    lava droplet (at 2,2,5), so the exterior surface area of the lava droplet
+    is 58.
+
+    What is the exterior surface area of your scanned lava droplet? */
+}
+
+impl Puzzle for Day18 {
+    fn part_one(&self, data: &'static str) -> String {
+        let cubes = data.lines().map(point_from_str).collect();
+
+        let surface_area = find_surface_area(&cubes);
+        surface_area.to_string()
+    }
+
+    fn part_two(&self, data: &'static str) -> String {
+        let mut cubes = data.lines().map(point_from_str).collect();
+        let air_pockets = find_air_pockets(&cubes);
+
+        cubes.extend(air_pockets.iter());
+        let surface_area = find_surface_area(&cubes);
+        surface_area.to_string()
+    }
+}
+
 fn get_neighbours(cubes: &HashSet<P3<i32>>, cube: P3<i32>) -> Vec<P3<i32>> {
     #[rustfmt::skip]
     let offsets = [
@@ -84,30 +165,22 @@ fn find_air_pockets(cubes: &HashSet<P3<i32>>) -> HashSet<P3<i32>> {
 
 fn point_from_str(s: &str) -> P3<i32> {
     let re = re!(r"(-?\d+).(-?\d+).(-?\d+)");
-    let Some(caps) = re.captures(s) else { panic!(r"Pattern (-?\d+).(-?\d+).(-?\d+) not recognised in input") };
-    let Some(nums) = caps.iter().skip(1).map(|cap| cap.map(|cap| cap.as_str())).collect_tuple() else {
+    let Some(caps) = re.captures(s) else {
+        panic!(r"Pattern (-?\d+).(-?\d+).(-?\d+) not recognised in input")
+    };
+    let Some(nums) = caps
+        .iter()
+        .skip(1)
+        .map(|cap| cap.map(|cap| cap.as_str()))
+        .collect_tuple()
+    else {
         panic!("Number of elements in string is incorrect")
     };
-    let (Some(x), Some(y), Some(z)) = nums else { panic!("Number of elements in string is incorrect") };
-    let (Ok(x), Ok(y), Ok(z)) = (x.parse(), y.parse(), z.parse()) else { panic!("Could not parse one or more elements") };
+    let (Some(x), Some(y), Some(z)) = nums else {
+        panic!("Number of elements in string is incorrect")
+    };
+    let (Ok(x), Ok(y), Ok(z)) = (x.parse(), y.parse(), z.parse()) else {
+        panic!("Could not parse one or more elements")
+    };
     P3 { x, y, z }
-}
-
-pub struct Day18 {}
-impl Puzzle for Day18 {
-    fn part_one(&self, data: &'static str) -> String {
-        let cubes = data.lines().map(point_from_str).collect();
-
-        let surface_area = find_surface_area(&cubes);
-        surface_area.to_string()
-    }
-
-    fn part_two(&self, data: &'static str) -> String {
-        let mut cubes = data.lines().map(point_from_str).collect();
-        let air_pockets = find_air_pockets(&cubes);
-
-        cubes.extend(air_pockets.iter());
-        let surface_area = find_surface_area(&cubes);
-        surface_area.to_string()
-    }
 }
